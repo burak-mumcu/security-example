b72fc8237d055b14fa9bb5054bb3d1439b00641d
Burak Mumcu <95939910+burak-mumcu@users.noreply.github.com>
2024-03-20 19:29:35 +0300

    feat:openssl keys alındı

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const path = require('path');
+const fs = require('fs')
 const express = require('express');
+const https = require('https');
 
 const PORT = 5000
 
 const app = express();
 
+app.get('/secret',(req,res) => {
+    return res.send('Your secret value is 25')
+})
+
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'public','index.html'));
 });
 
-app.listen(PORT, () => {
+https.createServer({
+    cert:fs.readFileSync('cert.pem'),
+    key:fs.readFileSync('key.pem'),
+}, app).listen(PORT, () => {
     console.log( `Server running on port ${PORT}`);
 })
