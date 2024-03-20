864480e7dac00ef42128350906f622395e6c71bf
Burak Mumcu <95939910+burak-mumcu@users.noreply.github.com>
2024-03-20 22:58:29 +0300

    feat: helmet entegration

--- server.js
+++ server.js
@@ -2,22 +2,25 @@ const fs = require('fs')
 const https = require('https');
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet')
 
 const PORT = 3000
 
 const app = express();
 
-app.get('/secret',(req,res) => {
+app.use(helmet());
+
+app.get('/secret', (req, res) => {
     return res.send('Your secret value is 25')
 })
 
-app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname,'public','index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 https.createServer({
-    key : fs.readFileSync('newkey.pem'),
-    cert : fs.readFileSync('cert.pem'),
+    key: fs.readFileSync('newkey.pem'),
+    cert: fs.readFileSync('cert.pem'),
 }, app).listen(PORT, () => {
-    console.log( `Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 })
