c01cbc172cff9fd8dd4e5d199fbc944b444b0d0e
Burak Mumcu <95939910+burak-mumcu@users.noreply.github.com>
2024-03-25 04:30:31 +0300

    feat: cookie session eklendi

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express');
 const helmet = require('helmet');
 const passport = require('passport')
 const { Strategy } = require('passport-google-oauth20');
+const cookieSession = require('cookie-session');
+
 require('dotenv').config()
 
 const PORT = 3000
@@ -12,6 +14,8 @@ const PORT = 3000
 const config = {
     CLIENT_ID : process.env.GOOGLE_CLIENT_ID,
     CLIENT_SECRET : process.env.GOOGLE_SECRET_ID,
+    COOKIE_KEY_1 : process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2 : process.env.COOKIE_KEY_2
 };
 
 const AUTH_OPTIONS = {
@@ -31,6 +35,12 @@ const app = express();
 
 app.use(helmet());
 
+app.use(cookieSession({
+    name:'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1,config.COOKIE_KEY_2]
+}))
+
 app.use(passport.initialize());
 
 function checkUser(req,res,next){
@@ -39,9 +49,6 @@ function checkUser(req,res,next){
         next()
 }
 
-function checkPermission (req,res,next){
-    // bu fonksiyon yetkilendirme kontrolleri için yazılacak
-}
 
 app.get('/auth/google',passport.authenticate('google',{
     scope:[ 'email','profile'],
